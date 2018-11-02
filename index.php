534c6a205205778fef0d97b8ae831cb2eece1e9c
HerrBrunke <34299221+HerrBrunke@users.noreply.github.com>
2018-11-02 11:05:53 +0100

    init
    helloWeb

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+ 
+ ?>
+
+ <!DOCTYPE html>
+ <html>
+ <head>
+     <meta charset="utf-8" />
+     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+     <title>SnakeBall</title>
+     <meta name="viewport" content="width=device-width, initial-scale=1">
+     <link rel="stylesheet" type="text/css" media="screen" href="style.css" />
+     <script src="snakeball.js"></script>
+ </head>
+ <body>
+    <div class="header">
+        <h1>
+            Welcome!        
+        </h1>
+    </div>
+    <div class="content">
+
+        <div class="game">
+            <div class="topMenu">
+                <ul>
+                    <li>Start</li>
+                    <li>Options</li>
+                    <li>Restart</li>
+                </ul>
+            </div>
+        </div>          
+    </div>      
+ </body>
+ </html>
